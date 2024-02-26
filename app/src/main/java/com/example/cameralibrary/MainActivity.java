09cd7f162d49ae9784a7028079e94f43192d1312
nabil <mynameisnabil09@gmail.com>
2024-02-26 15:09:32 +0700

    First Commit

---
+++ app/src/main/java/com/example/cameralibrary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cameralibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LogLib.printLog("MainActivity", "onCreate"); 
+    }
+}
---
+++ cameralibrary/src/main/java/com/example/cameralibrary/LogLib.java
@@ -0,0 +1,13 @@
+package com.example.cameralibrary;
+
+import android.util.Log;
+
+public class LogLib {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printLog(String tag, String message) {
+        Log.d(tag, message);
+    }
+}
